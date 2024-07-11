5a7703fac83617a50ed2eb984add8964a8b51ae8
jalumasa <jonathanalumasa@gmail.com>
2024-07-11 07:42:32 +0300

    Changed the line for the link

--- src/components/ResourceItem.js
+++ src/components/ResourceItem.js
@@ -5,7 +5,7 @@ const ResourceItem = ({ resource }) => {
     <div className="card">
       <h2>{resource.title}</h2>
       <p>{resource.description}</p>
-      <a href={resource.link} target="_blank" rel="noopener noreferrer">Learn More</a>
+      <a href={resource.link} target="_blank" rel="noopener noreferrer">Click here to learn more</a>
     </div>
   );
 };
