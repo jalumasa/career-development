c26edbb34a0579a0434e4e7a910fdeb0d4d38b28
Jonathan Alumasa <jonathanalumasa@gmail.com>
2024-07-16 22:48:29 +0300

    Updated firebase to include authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { addDoc, collection, getDocs, getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -14,6 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { addDoc, collection, db, getDocs };
-
+export { addDoc, auth, collection, db, getDocs };
