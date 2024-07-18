139a635345828dadbfc49b1bf6fa58514d5db0cd
Jonathan Alumasa <jonathanalumasa@gmail.com>
2024-07-18 20:19:42 +0300

    Created a section for users to check out their profile information

---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="container">
+      <h1>Profile</h1>
+      <p>Here is your profile information.</p>
+      {/* Add profile information here */}
+    </div>
+  );
+};
+
+export default Profile;
